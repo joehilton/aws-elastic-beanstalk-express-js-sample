3e10720d900a9a7123bcc07a815e618b37d70caf
Joe Hilton <hilton10@hotmail.co.uk>
2024-02-05 23:33:12 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Good Day to you!'));
+app.get('/', (req, res) => res.send('I love Mondays'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
